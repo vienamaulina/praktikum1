528b88f373fdbbcaf238d80f52aa07108205c103
viena maulina <vienamaulina@gmail.com>
2021-10-27 20:42:26 +0700

    Perubahan File

---
+++ tugas.py
@@ -0,0 +1,16 @@
+print("Menginput Biodata Diri")
+
+name=input("name:")
+call=input("call:")
+nim=input("nim:")
+address=input("address:")
+age=input("age:")
+dob=input("dob:")
+handphone=input("handphone:")
+
+print("assalamualaikum, hello everyone")
+print("let me introduce my self. my name is",name,"but you can call me",call,
+"my NIM is",nim,"i was born in",dob,"and i am",age,
+"years old. i am very glad if you want to invite my house in ",address,
+".so, dont forget to call me before with the handphone",handphone,"")
+print("thank you")
