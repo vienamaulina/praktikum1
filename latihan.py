db8f9585281964589f8ac432f00e89d4c44f7c40
viena maulina <vienamaulina@gmail.com>
2021-10-27 21:19:33 +0700

    Menambahkan file python

---
+++ latihan.py
@@ -0,0 +1,19 @@
+print("hello")
+print("Saya sedang belajar python")
+a = 8
+b = 6
+print ("variable a=",a)
+print ("variable b=",b)
+print ("hasil penjumlahan a+b=",a+b)
+
+a=int(input("masukan nilai/a:"))
+b=int(input("masukan nilai/b:"))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {1}&{0}=%d".format(a,b)%(a+b))
+
+#konversi nilai variable
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {1}+{0}=%d".format(a,b)%(a+b))
+print("hasil pembagian {1}/{0}=%d".format(a,b)%(a/b))
